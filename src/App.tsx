0bc2506384b53a3e12532c7dc6aeaa3d2b1e41fc
Joseph <jvp.2703@gmail.com>
2023-11-08 19:40:17 +0000

    structure fixing 1

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,51 @@ import { TSIcon } from "./icons/TSIcon";
 import { ReactIcon } from "./icons/ReactIcon";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { Fragment } from "react";
+
+const footerHeight = "35px"
 
 function App() {
   return (
-    <main className="min-h-screen flex flex-col">
-      <article className="pt-20 pb-12 px-5 flex flex-col">
+    <Fragment>
+      <main
+        className="pt-20 pb-14 px-5 container mx-auto"
+        style={{ minHeight: `calc(100vh - ${footerHeight})` }}
+      >
         <Header />
-        <section className="mx-auto container">
-          <div role="snippets-container" className="border rounded-md border-neutral-600/20 min-h-[350px] max-w-[850px] mx-auto bg-neutral-800/10 flex flex-col justify-center gap-10 items-center">
-            {snippets.map(
-              ({ title, githubLink, twPlaygroudLink, source, animation }) => (
-                <SnippetCard
-                  key={title}
-                  title={title}
-                  githubLink={githubLink}
-                  twPlaygroudLink={twPlaygroudLink}
-                  source={source}
-                  animation={animation}
-                />
-              )
-            )}
-          </div>
-        </section>
-        <span role="spacer" className="my-8"></span>
-        <section className="max-w-[650px] mx-auto flex justify-center flex-col">
-          <p className="flex text-emerald-200 justify-center items-center flex-row gap-4">
-            <TSIcon width="1.5em" height="1.5em" />
-            <ReactIcon width="1.5em" height="1.5em" />{" "}
-            <TWIcon width="1.5em" height="1.5em" />{" "}
-            <RadixIcon width="1.5em" height="1.5em" />{" "}
-          </p>
-        </section>
-      </article>
+        <article className="flex flex-col">
+          <section className="mx-auto container">
+            <div
+              role="snippets-container"
+              className="border rounded-md border-neutral-600/20 min-h-[350px] max-w-[850px] mx-auto bg-neutral-800/10 flex flex-col justify-center gap-10 items-center"
+            >
+              {snippets.map(
+                ({ title, githubLink, twPlaygroudLink, source, animation }) => (
+                  <SnippetCard
+                    key={title}
+                    title={title}
+                    githubLink={githubLink}
+                    twPlaygroudLink={twPlaygroudLink}
+                    source={source}
+                    animation={animation}
+                  />
+                )
+              )}
+            </div>
+          </section>
+          <span role="spacer" className="my-8"></span>
+          <section className="max-w-[650px] mx-auto flex justify-center flex-col">
+            <p className="flex text-emerald-200 justify-center items-center flex-row gap-4">
+              <TSIcon width="1.5em" height="1.5em" />
+              <ReactIcon width="1.5em" height="1.5em" />{" "}
+              <TWIcon width="1.5em" height="1.5em" />{" "}
+              <RadixIcon width="1.5em" height="1.5em" />{" "}
+            </p>
+          </section>
+        </article>
+      </main>
       <Footer />
-    </main>
+    </Fragment>
   );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   const now = new Date().getFullYear();
 
   return (
-    <footer className="mt-auto text-center py-2 font-mono text-[13px] px-5">
+    <footer className="text-center py-2 font-mono text-[13px] px-5">
       <div className="flex flex-row justify-between max-w-[650px] mx-auto ">
         <p className="opacity-40">By JosephV</p>
         <p className="opacity-40">{now} - Present</p>
