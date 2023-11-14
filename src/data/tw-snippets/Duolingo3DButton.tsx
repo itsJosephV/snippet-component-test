cbc93a1fa16799ac63ddfe277cdfe0e17a73c926
Joseph <jvp.2703@gmail.com>
2023-11-14 23:40:33 +0000

    button style fix 1

--- src/data/tw-snippets/Duolingo3DButton.tsx
+++ src/data/tw-snippets/Duolingo3DButton.tsx
@@ -5,8 +5,9 @@ export const Duolingo3DButton: TWSnippets = {
   githubLink: "https://github.com/itsJosephV",
   twPlaygroudLink: "https://play.tailwindcss.com/SpvojNOyWj",
   source: (
-    <button className="rounded-md bg-emerald-600 px-8 py-1.5 text-white antialiased shadow-[0_5px_0] shadow-emerald-800 active:translate-y-[5px] active:shadow-none transition-all duration-[.1s]">
+    <button className="rounded-md bg-emerald-600 px-8 py-1.5 text-white antialiased shadow-[0_5px_0] shadow-emerald-800 active:translate-y-[5px] active:shadow-none active:transition-all active:ease-in-out active:duration-[.1s]">
       Press me
     </button>
   ),
-};
+  
+};<div className="px-6 py-3 bg-gray-200 text-black rounded-lg border-b-4 border-b-gray-400 hover:border-b-0 transition-all ease-in-out duration-100">Hover over me</div>
