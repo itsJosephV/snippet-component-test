976b3ac6c954bc59a07c4517b06bcb39ee66367c
Joseph <jvp.2703@gmail.com>
2023-11-14 23:32:27 +0000

    button 3d snippet

--- src/data/snippets.tsx
+++ src/data/snippets.tsx
@@ -2,8 +2,9 @@ import { TWSnippets } from "../types";
 import { HoverArrowToRight } from "./tw-snippets/HoverArrowToRight";
 import { HoverUnderline } from "./tw-snippets/HoverUnderline";
 import { HoverColoredBorder } from "./tw-snippets/HoverColoredBorder"
+import { Duolingo3DButton } from "./tw-snippets/Duolingo3DButton";
 
 
-const snippets: TWSnippets[] = [HoverArrowToRight, HoverUnderline, HoverColoredBorder];
+const snippets: TWSnippets[] = [HoverArrowToRight, HoverUnderline, HoverColoredBorder, Duolingo3DButton];
 
 export default snippets;
---
+++ src/data/tw-snippets/Duolingo3DButton.tsx
@@ -0,0 +1,12 @@
+import { TWSnippets } from "../../types";
+
+export const Duolingo3DButton: TWSnippets = {
+  title: "Duolingo 3D button style",
+  githubLink: "https://github.com/itsJosephV",
+  twPlaygroudLink: "https://play.tailwindcss.com/SpvojNOyWj",
+  source: (
+    <button className="rounded-md bg-emerald-600 px-8 py-1.5 text-white antialiased shadow-[0_5px_0] shadow-emerald-800 active:translate-y-[5px] active:shadow-none transition-all duration-[.1s]">
+      Press me
+    </button>
+  ),
+};
