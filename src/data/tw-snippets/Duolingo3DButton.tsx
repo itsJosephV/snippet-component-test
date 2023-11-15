6dce64b8d6f79329b1a3fbff8bed8ca426c7c5a3
Joseph <jvp.2703@gmail.com>
2023-11-15 00:42:22 +0000

    New 3D Button - iOS Safari flicker fixed

--- src/data/tw-snippets/Duolingo3DButton.tsx
+++ src/data/tw-snippets/Duolingo3DButton.tsx
@@ -5,9 +5,11 @@ export const Duolingo3DButton: TWSnippets = {
   githubLink: "https://github.com/itsJosephV",
   twPlaygroudLink: "https://play.tailwindcss.com/SpvojNOyWj",
   source: (
-    <button className="rounded-md bg-emerald-600 px-8 py-1.5 text-white antialiased shadow-[0_5px_0] shadow-emerald-800 active:translate-y-[5px] active:shadow-none active:transition-all active:ease-in-out active:duration-[.1s]">
-      Press me
-    </button>
+    <button onTouchStart="" className="relative touch-auto">
+    <div className="absolute inset-x-0 -bottom-2 h-full rounded-lg bg-emerald-700"></div>
+
+    <div className="relative transform rounded-lg bg-emerald-500 px-10 py-2 text-white transition duration-200 active:translate-y-2">Press me</div>
+  </button>
   ),
   
 };<div className="px-6 py-3 bg-gray-200 text-black rounded-lg border-b-4 border-b-gray-400 hover:border-b-0 transition-all ease-in-out duration-100">Hover over me</div>
